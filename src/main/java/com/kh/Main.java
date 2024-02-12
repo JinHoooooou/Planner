2843e376997f3270c6a623710e466562fa49dd63
JinHoooooou <jinho4744@naver.com>
2024-02-12 19:40:45 +0900

    modify gradle.build

--- src/main/java/com/kh/Main.java
+++ src/main/java/com/kh/Main.java
@@ -7,12 +7,5 @@ public class Main {
   public static void main(String[] args) throws Exception {
     WebServer webServer = new WebServer();
     webServer.start(args);
-//    if (args[0].equals("web")) {
-//      WebServer webServer = new WebServer();
-//      webServer.start(args);
-//    } else {
-//      MainView mainView = new MainView();
-//      mainView.execute();
-//    }
   }
 }
