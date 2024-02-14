4e3b3f18006e91422ed80f1c6385784b7132a745
JinHo Lee <jinho4744@naver.com>
2024-02-14 14:24:01 +0900

    Use Tomcat instead of WebServer

--- src/main/java/com/kh/Main.java
+++ src/main/java/com/kh/Main.java
@@ -1,11 +1,33 @@
 package com.kh;
 
-import com.kh.server.WebServer;
+import java.io.File;
+import org.apache.catalina.Context;
+import org.apache.catalina.WebResourceRoot;
+import org.apache.catalina.startup.Tomcat;
+import org.apache.catalina.webresources.DirResourceSet;
+import org.apache.catalina.webresources.StandardRoot;
+
 
 public class Main {
 
+  private static final String WEB_APP_DIR_LOCATION = "src/main/webapp";
+  private static final String CLASS_LOCATION = "out/production/classes";
+  private static final String WEB_APP_MOUNT = "/WEB-INF/classes";
+
   public static void main(String[] args) throws Exception {
-    WebServer webServer = new WebServer();
-    webServer.start(args);
+    Tomcat tomcat = new Tomcat();
+    tomcat.setPort(8080);
+
+    Context context = tomcat.addWebapp("/", new File(WEB_APP_DIR_LOCATION).getAbsolutePath());
+    File additionWebInfClasses = new File(CLASS_LOCATION);
+    WebResourceRoot resources = new StandardRoot(context);
+    resources.addPreResources(
+        new DirResourceSet(resources, WEB_APP_MOUNT, additionWebInfClasses.getAbsolutePath(), "/"));
+    context.setResources(resources);
+
+    tomcat.getConnector();
+
+    tomcat.start();
+    tomcat.getServer().await();
   }
 }
