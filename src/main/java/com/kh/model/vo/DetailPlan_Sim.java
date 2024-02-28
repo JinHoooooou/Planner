e99c65263d04202fb734ac47e24d092beec8598e
smjae96 <tlawodbs0528@naver.com>
2024-02-28 22:17:58 +0900

    0228 commit

---
+++ src/main/java/com/kh/model/vo/DetailPlan_Sim.java
@@ -0,0 +1,36 @@
+package com.kh.model.vo;
+
+import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Builder
+public class DetailPlan_Sim {
+	private int detailPlanId;
+	private int planId;
+	private String writer;
+	private String contents;
+	private Date startTime;
+	private Date endTime;
+	private Date remindAlarmTime;
+	private String complete;
+	
+	  public static DetailPlan_Sim from(ResultSet resultSet) throws SQLException {
+		    return DetailPlan_Sim.builder()
+		        .detailPlanId(resultSet.getInt("detaiil_plan_id"))
+		        .planId(resultSet.getInt("plan_id"))
+		        .writer(resultSet.getString("writer"))
+		        .contents(resultSet.getString("contents"))
+		        .startTime(resultSet.getDate("start_time"))
+		        .endTime(resultSet.getDate("end_time"))
+		        .remindAlarmTime(resultSet.getDate("remind_alarm_time"))
+		        .build();
+		  }
+}
