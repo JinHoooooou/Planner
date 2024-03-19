011db44483f6d211ea304217f760a3576e9bc7fe
jw <superok96@gmail.com>
2024-03-19 17:45:10 +0900

    서블릿연동

--- src/main/java/com/kh/Main.java
+++ src/main/java/com/kh/Main.java
@@ -11,7 +11,7 @@ import org.apache.catalina.webresources.StandardRoot;
 public class Main {
 
   private static final String WEB_APP_DIR_LOCATION = "src/main/resources";
-  private static final String CLASS_LOCATION = "/bin";
+  private static final String CLASS_LOCATION = "bin/";
   private static final String WEB_APP_VIRTUAL_PATH = "/WEB-INF/classes";
 
   public static void main(String[] args) throws Exception {
