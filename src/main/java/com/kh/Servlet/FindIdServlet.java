8733df8c6e822ffc6db91db451d44bb865922369
minsock <serominsockji9n9@gmail.com>
2024-03-20 14:15:01 +0900

    Planner

---
+++ src/main/java/com/kh/Servlet/FindIdServlet.java
@@ -0,0 +1,31 @@
+package com.kh.Servlet;
+
+import java.io.IOException;
+
+import com.kh.model.dao.UserDao;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+@WebServlet("/user/findid")
+public class FindIdServlet extends HttpServlet {
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String userName = req.getParameter("userName");
+        String email = req.getParameter("email");
+        String phone = req.getParameter("phone");
+        try {
+            new UserDao().findId(userName, email, phone);
+            
+            resp.setStatus(HttpServletResponse.SC_OK);
+        } catch (IllegalArgumentException e) {
+            resp.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        }
+
+    }
+    
+}
---
+++ src/main/java/com/kh/Servlet/FindPwServlet.java
@@ -0,0 +1,18 @@
+package com.kh.Servlet;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/user/findid")
+public class FindPwServlet extends HttpServlet {
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    
+    }
+    
+}
